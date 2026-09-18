965561377e7389f4ed5e1f76e20986825c5a2056
KhunAH156 <2604762@sit.singaporetech.edu.sg>
2026-09-18 10:34:20 +0800

    Implemented get valid input function

---
+++ Week3/Modular_auditor.py
@@ -0,0 +1,49 @@
+inventory = 0
+failed_attempts = 0
+print("-----------------------------------")
+print("Welcome to the Inventory Auditor!")
+print("-----------------------------------")
+
+def get_valid_input():
+    while True:
+        user_input = input("Enter inventory amount or 'quit' to exit: ")
+
+        if user_input.lower() == "quit":
+            return "quit"
+
+        try:
+            value = int(user_input)
+
+            if value < 0:
+                print("Input cannot be negative. Please enter a valid number.")
+                continue
+
+            return value
+
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+while (True):
+    user_input = input("Enter inventory amount or 'quit' to exit: ")
+    if user_input.lower() == 'quit':
+        print(f"Exiting... Total inventory: {inventory}, Failed attempts: {failed_attempts}")
+        break
+    try:
+        
+        if int(user_input) < 0:
+            print("Input cannot be negative. Please enter a valid number.")
+            failed_attempts += 1
+            print(f"Current inventory: {inventory}")
+        else:
+            inventory += int(user_input)
+            print(f"Current inventory: {inventory}")
+
+        if inventory >= 500:
+            print(f"Inventory limit reached: {inventory}. Cannot add more items.")
+            inventory -= int(user_input)
+            break
+
+    except ValueError:
+        print("Invalid input. Please enter a valid number for inventory amount.")
+        failed_attempts += 1
+        print(f"Current inventory: {inventory}")
